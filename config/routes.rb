e59fd26beba446e8c9a87f74551a9de12088e118
brooks-brothers-007 <shoutanakane@gmail.com>
2020-12-18 17:13:33 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :messages
   devise_for :users
   root to: "rooms#index"
 
-  resources :users, only: [:edit, :update,]
-  resources :rooms, only: [:new, :create]
+  resources :users, only: [:edit, :update, ]
+  resources :rooms, only: [:new, :create ] do
+    resources :messages, only: [:index, :create]
+  end
+
 end
--- db/migrate/20201218054234_create_messages.rb
+++ db/migrate/20201218054234_create_messages.rb
@@ -1,6 +1,7 @@
-class CreateRoomUsers < ActiveRecord::Migration[6.0]
+class CreateMessages < ActiveRecord::Migration[6.0]
   def change
-    create_table :room_users do |t|
+    create_table :messages do |t|
+      t.string  :content
       t.integer :room_id, foreign_key: true
       t.integer :user_id, foreign_key: true
       t.timestamps
