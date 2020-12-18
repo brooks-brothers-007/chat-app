87ad5e36a24db7cbf211ecea000d4d1c69e7eec9
brooks-brothers-007 <shoutanakane@gmail.com>
2020-12-18 12:07:08 +0900

    messagesテーブルの作成

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,9 @@
-class User < ApplicationRecord
+class Message < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :room_users
-  has_many :rooms, through: :room_users
-  has_many :messages
-
-  validates :name, presence: true
+         belongs_to :room
+         belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_070750) do
+ActiveRecord::Schema.define(version: 2020_12_18_024147) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_messages_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_messages_on_reset_password_token", unique: true
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id"
@@ -40,6 +57,8 @@ ActiveRecord::Schema.define(version: 2020_12_17_070750) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
 end
